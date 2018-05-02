e99a50014cc030f8bf6fb150367f390a6bef6578
artim-anton <artim-anton@yandex.ru>
2018-05-02 14:49:20 +0300

    add Handler

--- app/src/main/java/com/artimanton/coinmarketcap/MainActivity.kt
+++ app/src/main/java/com/artimanton/coinmarketcap/MainActivity.kt
@@ -2,13 +2,19 @@ package com.artimanton.coinmarketcap
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import com.artimanton.coinmarketcap.adapters.CoinAdapter
 import com.artimanton.coinmarketcap.api.ServiceGenerator
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter:CoinAdapter
@@ -20,6 +26,14 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter=adapter
 
         loadCoins()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({
+            loadCoins()
+            Toast.makeText(this@MainActivity, "Обновление денных", Toast.LENGTH_LONG).show()
+        }, 5000)
 
     }
 
